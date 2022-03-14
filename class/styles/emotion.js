55169f3a5c5e1290d8ebd10754436ea2024c2af4
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-03-14 18:25:56 +0900

    첫 잔디

---
+++ class/styles/emotion.js
@@ -0,0 +1,22 @@
+import styled from '@emotion/styled'
+
+
+export const MyTitle = styled.div`
+color: black;
+`
+
+export const MyInput = styled.div`
+
+`
+
+export const MyLable = styled.div`
+display=flex;
+flex-direction: column;
+justify-content: column;
+`
+export const MyId = styled.input`
+height: 30px;
+`
+export const MyPassword = styled.input`
+height: 30px;
+`
---
+++ programmers/로봇계단.js
@@ -0,0 +1,20 @@
+// function solution(){
+
+// let answer = 0;
+// let current = 1;
+// const limit = 100;
+
+// for(let i=1; i<limit;i=i+2){
+//   answer=answer+1;
+// }
+
+// if(current!== 100){
+//    current=current+1;
+//    answer = answer+1;
+//    }
+
+// return answer;
+// }
+
+const limit = 100;
+const answer = Math.floor(limit/2);
