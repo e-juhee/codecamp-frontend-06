4c9990085f2f9855f1f777751e17f9eecab7efcf
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-03-16 23:20:27 +0900

    update freeboard react useState to react-hook-form

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -5,79 +5,36 @@ import {Wrapper,Title, Input, InputShort, InputshortWrapper,
     Search, PictureWrapper, Picture, PictureIcon, PictureLabel, 
     RadioWrapper, Radio, RadioLabel, Create, Error
 } from "../../../styles/createBoard"
-import { useState } from 'react'
+
+import { useForm } from 'react-hook-form';
 
 export default function createBoard() {
 
-    const [writer, setWriter] = useState("")
-    const [writerError, setWriterError] = useState("")
-    const [password, setPassword] = useState("")
-    const [passwordError, setPasswordError] = useState("")
-    const [contentTitle, setContentTitle] = useState("")
-    const [contentTitleError, setContentTitleError] = useState("")
-    const [content, setContent] = useState("")
-    const [contentError, setContentError] = useState("")
     
-    function onChangeWriter(event){
-        setWriter(event.target.value)
-    }
-    function onChangePassword(event){
-        setPassword(event.target.value)
-    }
-    function onChangeContentTitle(event){
-        setContentTitle(event.target.value)
-    }
-    function onChangeContent(event){
-        setContent(event.target.value)
-    }
-
-    let isValid = true
-    function onClickButton(){
-        if(writer === ""){
-            setWriterError("이름을 입력해주세요.")
-            isValid = false
-        } else {
-            setWriterError("")
-        }
-        if(password === ""){
-            setPasswordError("비밀번호를 입력해주세요.")
-            isValid = false
-        } else {
-            setPasswordError("")
-        }
-        if(contentTitle === ""){
-            setContentTitleError("제목을 입력해주세요.")
-            isValid = false
-        } else {
-            setContentTitleError("")
-        }
-        if(content === ""){
-            setContentError("내용을 입력해주세요.")
-            isValid = false
-        } else {
-            setContentError("")
-        }
-        if(isValid === true){
-            alert("게시글이 등록되었습니다.")
-        }
+    //react-hook-form: 에러메세지 표시
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data); //input 안의 값
+        alert("게시글이 등록되었습니다.");
+        
     }
 
 
   return (
-    <Wrapper>
+    <Wrapper onSubmit={handleSubmit(onSubmit)}>
         <Title>게시물 등록</Title>
         <Input>
             <InputShort>
                 <InputshortWrapper>
                     <InputLabel>작성자<Star>*</Star></InputLabel>
-                    <Writer onChange={onChangeWriter} type="text" placeholder="이름을 적어주세요."/>
-                    <Error>{writerError}</Error>
+                    <Writer {...register("writer", { required: true })}   type="text" placeholder="이름을 적어주세요."/>
+                    {errors.writer && <Error>이름을 입력해주세요.</Error>}
                 </InputshortWrapper>
            
                 <InputshortWrapper>
                     <InputLabel>비밀번호<Star>*</Star></InputLabel>
-                    <Password onChange={onChangePassword} type="text" placeholder="비밀번호를 입력해주세요."/>
-                    <Error>{passwordError}</Error>
+                    <Password {...register("password", { required: true })}  type="password" placeholder="비밀번호를 입력해주세요."/>
+                    {errors.password && <Error>비밀번호를 입력해주세요.</Error>}
                 </InputshortWrapper>
             </InputShort>
         </Input>
@@ -85,11 +42,11 @@ export default function createBoard() {
         <Input>
             <InputLong>
                 <InputLabel>제목<Star>*</Star></InputLabel>
-                <ContentTitle onChange={onChangeContentTitle}  type="text" placeholder="제목을 작성해주세요."/>
-                <Error>{contentTitleError}</Error>
+                <ContentTitle {...register("contentTitle", { required: true })}   type="text" placeholder="제목을 작성해주세요."/>
+                {errors.contentTitle && <Error>제목을 입력해주세요.</Error>}
                 <InputLabel>내용<Star>*</Star></InputLabel>
-                <Content onChange={onChangeContent}  type="text" placeholder="내용을 작성해주세요."/>
-                <Error>{contentError}</Error>
+                <Content {...register("title", { required: true })}  type="text" placeholder="내용을 작성해주세요."/>
+                {errors.title && <Error>내용을 입력해주세요.</Error>}
             </InputLong>
         </Input>
 
@@ -97,18 +54,18 @@ export default function createBoard() {
             <InputLong>
                 <InputLabel>주소</InputLabel>
                 <InputAddress>
-                    <InputInputAddress type="text" placeholder="07250"/>
+                    <InputInputAddress {...register("zipcode")} type="text" placeholder="07250"/>
                     <Search>우편번호 검색</Search>
                 </InputAddress>
-                <AddressDetail type="text"/>
-                <AddressDetail type="text"/>
+                <AddressDetail {...register("address1")} type="text"/>
+                <AddressDetail {...register("address2")} type="text"/>
             </InputLong>
         </Input>
 
         <Input>
             <InputLong>
                 <InputLabel>유튜브</InputLabel>
-                <InputInput type="text" placeholder="링크를 복사해주세요."/>
+                <InputInput {...register("link")} type="text" placeholder="링크를 복사해주세요."/>
             </InputLong>
         </Input>
 
@@ -137,18 +94,18 @@ export default function createBoard() {
                 <InputLabel>메인 설정</InputLabel>
                 <RadioWrapper>
                     <RadioLabel>
-                        <Radio type="radio" value="youtube" name="main" checked/>
+                        <Radio {...register("youtube")} type="radio" value="youtube" name="main" checked/>
                         유튜브
                     </RadioLabel>
                     <RadioLabel>
-                        <Radio type="radio" value="picture" name="main" />
+                        <Radio {...register("image")} type="radio" value="picture" name="main" />
                         사진
                     </RadioLabel>
                 </RadioWrapper>
             </InputLong>
         </Input>
 
-        <Create onClick={onClickButton}>등록하기</Create>
+        <Create>등록하기</Create>
 
     </Wrapper>
   )
