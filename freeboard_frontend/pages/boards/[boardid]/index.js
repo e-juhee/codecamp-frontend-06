645c60993d082e1a7f201e0d9672b7d72b562263
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-03-21 10:40:11 +0900

    Chane directory name and query ID for unity

--- freeboard_frontend/pages/boards/[boardid]/index.js
+++ freeboard_frontend/pages/boards/[boardid]/index.js
@@ -65,7 +65,7 @@ export default function BoardsDetailPage(){
 
     const router = useRouter()
     const { data } = useQuery(FETCH_BOARD, {
-        variables: {boardId : router.query.boardid} //폴더명
+        variables: {boardId : router.query.boardId} //폴더명
     })
 
     /*ToolTip show & hide*/
