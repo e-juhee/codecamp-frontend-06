6dff048322d8d36a99776541a0192d4bf1ba3e7b
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-03-23 19:35:20 +0900

    Day8 Algorithm Class

--- programmers/09-문자열 다루기.js
+++ programmers/09-문자열 다루기.js
@@ -6,3 +6,31 @@ function solution(s) {
     }
     return answer;
 }
+
+
+function solution(s) {
+    if(s.length !== 4 && s.length !==6){
+        return false;
+    }
+    for(let i=0; i<s.length; i++){
+        if(isNaN(s[i])===true){
+            return false;
+        }
+    }
+    return true;
+}
+
+function solution(s) {
+    if(s.length !== 4 && s.length !==6){
+        return false;
+    }
+    const answer = s.split("") //배열로 만들어줌
+                     .filter(num => {
+                         //데이터가 숫자가 아닌 문자일 경우만 남긴다.
+                         //isNaN의 결과가 true인 경우 (NaN 값인 경우)
+                         console.log(num, isNaN(num))
+                        return isNaN(num) === true
+                       }) 
+    console.log(answer)
+    return answer.length === 0
+}
--- programmers/09-약수의 합.js
+++ programmers/09-약수의 합.js
@@ -9,6 +9,35 @@ function solution(n) {
     return answer;
 }
 
+function solution(n) {
+    let answer = n;
+    for(let i=1;i<=n/2;i++){
+        if(n%i===0){
+            answer = answer + i
+        }
+    }   
+    return answer;
+}
+
+function solution(n) {
+    const answer = new Array(n)
+                    .fill(1)
+                    .reduce((acc,cur,i)=>{
+                        return n % (cur+i) === 0
+                            //약수가 맞다면, 약수를 더한 값을 다음으로 넘겨주고
+                            ? acc + (cur+i)
+                            //약수가 아니라면, 더하지 않고 그냥 다음으로 넘겨준다.
+                            : acc
+                    },0 )
+    return answer;
+}
+
+// 배열이 비어있으면(undefined) 메소드를 사용할 수 없다.
+// 그래서 fill을 사용해서 배열을 채워준다.
+// 기억해두자!! new Array(n).fill(1)
+// 기억해두자! i를 추가하면 index를 활용할 수 있다.
+
+
 /*
 약수의 합
 문제 설명
