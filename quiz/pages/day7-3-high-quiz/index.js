092408e8efc4d9037c0c77ef5ad61ba7282a6e3b
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-03-24 00:29:58 +0900

    Checkbox...

--- quiz/pages/day7-3-high-quiz/index.js
+++ quiz/pages/day7-3-high-quiz/index.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { useState } from 'react'
 
 const Row = styled.div`
 display: flex;
@@ -13,53 +14,71 @@ width:25%;
 background-color: lightgray;
 `
 
-
 export default function Day7QuizPage(){
+
+        
+        const dataList = [
+            {id:1, title:"9월달 시스템 점검 안내입니다.", date: "2022.09.19"},
+            {id:2, title:"안녕하세요! 공지사항 전달드립니다.", date: "2022.09.17"},
+            {id:3, title:"개인정보 처리방침 변경 사전 안내", date: "2022.09.15"},
+            {id:4, title:"iOS 10.0 이하 지원 중단 안내", date: "2022.09.13"},
+            {id:5, title:"이용약관 변경 사전 안내", date: "2022.09.10"},
+            {id:6, title:"개인정보 처리방침 변경 사전 안내", date: "2022.08.19"},
+        ]
+
+
+        const [checkedList, setCheckedList] = useState([]);
+        const [one, setOne] = useState();
+        const [all, setAll] = useState();
+
+
+        const onCheckedAll = (e) => {
+            console.log(e)
+            // console.log(checkedList.includes(1))
+            // setCheckedList(e.target.checked ? dataList : [])
+            console.log(checkedList)
+            // e.target.checked ? checkedListLength=6 : checkedListLength=0;
+            checkedList.length===6 ? setAll("true") : setAll("false")
+            console.log(checkedList.length)
+            //리스트에 몇개들어왓는지 확인해서, 총 길이랑 같으면 true ㅇ아니면 ㄴㄴ
+        }
+
+        const onCheckedOne = (e) => {
+            console.log(e)
+            console.log(e.target)
+            let a;
+            e.target.checked ? checkedList.push(e.target.id) : checkedList.pop()
+            console.log(checkedList.length)
+            checkedList.length === 6 ? setAll(true): setAll(false)
+            console.log(checkedList)
+        }
+
+   
+
 
     return (
         <>
-                <Row>
-                    <Head><input type="checkbox"/></Head>
+                <Row key="0">
+                    <Head><input type="checkbox"
+                                onChange={onCheckedAll}
+                                checked={all}/>
+                    </Head>
                     <Head>번호</Head>
                     <Head>제목</Head>
                     <Head>작성일</Head>
                 </Row>
-                <Row>
-                    <Column><input type="checkbox" /></Column>
-                    <Column>1</Column>
-                    <Column>제목6</Column>
-                    <Column>2022.03.23</Column>
-                </Row>
-                <Row>
-                    <Column><input type="checkbox" /></Column>
-                    <Column>2</Column>
-                    <Column>제목5</Column>
-                    <Column>2022.03.22</Column>
-                </Row>
-                <Row>
-                    <Column><input type="checkbox" /></Column>
-                    <Column>3</Column>
-                    <Column>제목4</Column>
-                    <Column>2022.03.21</Column>
-                </Row>
-                <Row>
-                    <Column><input type="checkbox" /></Column>
-                    <Column>4</Column>
-                    <Column>제목3</Column>
-                    <Column>2022.03.20</Column>
-                </Row>
-                <Row>
-                    <Column><input type="checkbox" /></Column>
-                    <Column>5</Column>
-                    <Column>제목2</Column>
-                    <Column>2022.03.19</Column>
-                </Row>
-                <Row>
-                    <Column><input type="checkbox" /></Column>
-                    <Column>6</Column>
-                    <Column>제목1</Column>
-                    <Column>2022.03.18</Column>
-                </Row>
+
+                {dataList.map((el) => (
+                        <Row key={el.id} >
+                            <Column><input id={el.id} type="checkbox"
+                                            onChange={onCheckedOne}
+                                            checked={one}/>
+                            </Column>
+                            <Column>{el.id}</Column>
+                            <Column>{el.title}</Column>
+                            <Column>{el.date}</Column>
+                        </Row>
+                ))}
         </>
     )
 }
