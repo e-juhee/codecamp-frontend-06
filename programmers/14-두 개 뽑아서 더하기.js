b93f0480314615f6ff516b765d80ff4d0355cc81
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-03-31 18:14:58 +0900

    Day14 Algorithm forEach & Set

--- programmers/14-두 개 뽑아서 더하기.js
+++ programmers/14-두 개 뽑아서 더하기.js
@@ -11,3 +11,39 @@ function solution(numbers) {
   }
   return answer.sort((a, b) => a - b);
 }
+
+// 이중 for문
+function solution(numbers) {
+  const answer = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const sum = numbers[i] + numbers[j];
+      if (answer.includes(sum) === false) answer.push(sum);
+    }
+  }
+  return answer.sort((a, b) => a - b);
+}
+
+// Set
+function solution(numbers) {
+  const answer = new Set();
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const sum = numbers[i] + numbers[j];
+      answer.add(sum);
+    }
+  }
+  return [...answer].sort((a, b) => a - b);
+}
+
+// forEach & Set
+function solution(numbers) {
+  const answer = new Set();
+  numbers.forEach((num1, i) => {
+    numbers.slice(i + 1).forEach((num2) => {
+      const sum = num1 + num2;
+      answer.add(sum);
+    });
+  });
+  return [...answer].sort((a, b) => a - b);
+}
