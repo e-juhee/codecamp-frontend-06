8434f32fc97af011c091936f89b8132998e664a5
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-04-04 00:46:56 +0900

    Install ESLint, Prettier, Husky

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -1,6 +1,5 @@
-// import "../styles/globals.css";
+// import '../styles/globals.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import "antd/dist/antd.css";
 import { AppProps } from "next/app";
 import Layout from "../src/components/commons/layout";
 import { globalStyles } from "../src/commons/styles/globalStyles";
@@ -8,10 +7,10 @@ import { Global } from "@emotion/react";
 import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  //Component, pageProps는 Next.js가 처음에 넣어주는 데이터! Docs를 보고 타입(AppProps)을 지정해주면 된다.
+  // Component, pageProps는 Next.js가 처음에 넣어주는 데이터! Docs를 보고 타입(AppProps)을 지정해주면 된다.
   const client = new ApolloClient({
-    uri: "http://backend06.codebootcamp.co.kr/graphql", //백엔드 컴퓨터의 주소를 알려줌
-    cache: new InMemoryCache(), //inMemoryCache에 저장한다. 메모리에 저장하면 컴퓨터를 껐다가 키면 날아감
+    uri: "http://backend06.codebootcamp.co.kr/graphql", // 백엔드 컴퓨터의 주소를 알려줌
+    cache: new InMemoryCache(), // nMemoryCache에 저장한다. 메모리에 저장하면 컴퓨터를 껐다가 키면 날아감
   });
 
   return (
--- freeboard_frontend/styles/boards/new.js
+++ freeboard_frontend/styles/boards/new.js
@@ -1,12 +1,4 @@
-import styled from '@emotion/styled'
-
-//전체 선택자는 없을까?!
-// styled.body`
-//   box-sizing: border-box;
-//   margin: 0px;
-// `
-  //box-sizing: border-box;
-  //margin: 0px;
+import styled from "@emotion/styled";
 
 export const Wrapper = styled.form`
   display: flex;
@@ -14,79 +6,79 @@ export const Wrapper = styled.form`
   justify-content: center;
   align-items: center;
   width: 1200px;
-  margin:100px auto;
+  margin: 100px auto;
   padding: 60px 100px 100px 100px;
   border: 1px solid gray;
-`
+`;
 export const Title = styled.div`
-  font-family: 'Noto Sans CJK KR';
+  font-family: "Noto Sans CJK KR";
   font-size: 36px;
   font-weight: 700;
-`
+`;
 export const WriterWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
   padding-top: 40px;
-`
+`;
 
 export const InputWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-`
+`;
 export const Label = styled.div`
   display: flex;
   justify-content: row;
   padding: 25px 0px 16px 0px;
   font-size: 16px;
   line-height: 16px;
-`
+`;
 export const Required = styled.div`
   padding-left: 5px;
   color: hotpink;
-`
+`;
 export const ShortInput = styled.input`
   width: 495px;
   height: 52px;
   padding: 14px;
-  border: 1px solid #BDBDBD;
+  border: 1px solid #bdbdbd;
   border-radius: 5px;
-`
+`;
 export const Error = styled.div`
   padding-left: 5px;
   color: red;
   font-size: 11px;
-`
+`;
 export const Input = styled.input`
   width: 100%;
   height: 52px;
   padding: 14px;
-  border: 1px solid #BDBDBD;
+  border: 1px solid #bdbdbd;
   border-radius: 5px;
-`
+`;
 export const ContentInput = styled.textarea`
   width: 100%;
   height: 480px;
   padding: 14px;
-  border: 1px solid #BDBDBD;
+  border: 1px solid #bdbdbd;
   border-radius: 5px;
-`
+`;
 export const ZipCodeWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   width: 217px;
-`
+`;
 export const ZipCode = styled.input`
   width: 77px;
   height: 52px;
   margin-bottom: 16px;
   padding: 14px;
-  border: 1px solid #BDBDBD;
+  border: 1px solid #bdbdbd;
   border-radius: 5px;
-`
+`;
 export const ZipCodeButton = styled.button`
   width: 124px;
   height: 52px;
@@ -95,39 +87,39 @@ export const ZipCodeButton = styled.button`
   background: green;
   color: white;
   cursor: pointer;
-`
+`;
 export const Address = styled.input`
   height: 52px;
   margin-bottom: 16px;
-  border: 1px solid #BDBDBD;
+  border: 1px solid #bdbdbd;
   border-radius: 5px;
-`
+`;
 export const ImageWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   width: 282px;
   padding-bottom: 40px;
-`
+`;
 export const ImageButton = styled.button`
   width: 78px;
   height: 78px;
-  background: #BDBDBD;
+  background: #bdbdbd;
   border: none;
   border-radius: 5px;
   cursor: pointer;
-`
+`;
 export const ImageIcon = styled.div`
   padding-bottom: 5px;
   font-size: 28px;
-`
+`;
 export const ImageLabel = styled.div`
-  font-family: 'Noto Sans CJK KR';
+  font-family: "Noto Sans CJK KR";
   font-size: 13px;
-`
+`;
 export const SettingWrapper = styled.div`
   width: 100%;
-`
+`;
 export const Radio = styled.input`
   cursor: pointer;
   appearance: none;
@@ -138,18 +130,18 @@ export const Radio = styled.input`
 
   &:checked {
     appearance: none;
-    background: #FFD600;
-    }
-`
+    background: #ffd600;
+  }
+`;
 export const RadioLabel = styled.label`
   padding-right: 22px;
-`
+`;
 export const SubmitButton = styled.button`
   width: 179px;
   height: 52px;
   border: none;
   border-radius: 5px;
-  background: #FFD600;
+  background: #ffd600;
   font-size: 16px;
   font-weight: 500;
   cursor: pointer;
@@ -160,4 +152,4 @@ export const SubmitButton = styled.button`
     font-weight: 900;
     letter-spacing: 5px;
   }
-`
+`;
