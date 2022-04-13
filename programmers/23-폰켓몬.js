56ddc13aef2ae3f324dd9d81965158426eef3d8e
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-04-13 18:19:53 +0900

    Algorithm poketmon

--- programmers/23-폰켓몬.js
+++ programmers/23-폰켓몬.js
@@ -3,3 +3,34 @@ function solution(nums) {
     ? nums.length / 2
     : new Set(nums).size;
 }
+
+function solution(nums) {
+  const answer = [];
+  for (let i = 0; i < nums.length; i++) {
+    console.log(answer, nums[i]);
+    if (answer.length < nums.length / 2 && answer.includes(nums[i]) === false) {
+      answer.push(nums[i]);
+    }
+  }
+  return answer.length;
+}
+
+function solution(nums) {
+  const answer = new Set([]);
+  for (let i = 0; i < nums.length; i++) {
+    if (answer.size < nums.length / 2) {
+      answer.add(nums[i]);
+    }
+  }
+  return answer.size;
+}
+
+function solution(nums) {
+  const answer = new Set([]);
+  nums.forEach((monster) => {
+    if (answer.size < nums.length / 2) {
+      answer.add(monster);
+    }
+  });
+  return answer.size;
+}
