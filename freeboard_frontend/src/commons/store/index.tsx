888dbe0c1bf69f3f4bc4d32d2d62c559f4f1f434
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-05-03 11:11:25 +0900

    build

--- freeboard_frontend/src/commons/store/index.tsx
+++ freeboard_frontend/src/commons/store/index.tsx
@@ -1,6 +1,17 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { getAccessToken } from "../libraries/getAccessToken";
 
 export const accessTokenState = atom({
   key: "accessTokenSate",
   default: "",
+});
+
+// atom은 특정 데이터, selector는 기능이 들어갈 수 있다.
+// 이 함수를 여러 컴포넌트에서 공유할 수 있게 된다. like 글로벌 함수!
+export const restoreAccessTokenLoadable = selector({
+  key: "restoreAccessTokenLoadable",
+  get: async () => {
+    const newAccessToken = await getAccessToken();
+    return newAccessToken;
+  },
 });
--- freeboard_frontend/src/components/commons/hocs/withAuth.tsx
+++ freeboard_frontend/src/components/commons/hocs/withAuth.tsx
@@ -1,40 +1,3 @@
-// import { useRouter } from "next/router";
-// import { useEffect } from "react";
-// import { useRecoilState } from "recoil";
-// import { getAccessToken } from "../../../commons/libraries/getAccessToken";
-// import { accessTokenState } from "../../../commons/store";
-
-// @ts-ignore
-
-// export default function useAuth() {
-//   const router = useRouter();
-//   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
-//   useEffect(() => {
-//     console.log(accessToken);
-//     if (!accessToken) {
-//       console.log("토큰없더");
-//       // console.log(accessToken);
-//       // Modal.error({ content: "로그인 후 이용 가능합니다." });
-//       // router.push("/login");
-//       /* accessToken 재발급 받아서 state에 넣어주기 */
-//       getAccessToken()
-//         .then((res) => {
-//           setAccessToken(res);
-//           console.log(res);
-//         })
-//         .catch((e) => {
-//           console.log(e);
-//           alert("로그인하셈");
-//           router.push("/login");
-
-//           // console.log(newAccessToken);
-//         });
-//     }
-//   }, []);
-
-//   return {};
-// }
-
 /* 이게 원래 내 코드 */
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -44,22 +7,9 @@ import { accessTokenState } from "../../../commons/store";
 // @ts-ignore
 export const withAuth = (Component) => (props) => {
   const [accessToken] = useRecoilState(accessTokenState);
+  // const restoreAccessToken = useRecoilValueLoadable(restoreAccessTokenLoadable);
 
   const router = useRouter();
-
-  /* 권한 분기 */
-  // useEffect(() => {
-  //   if (!accessToken) {
-  //     getAccessToken().then((newAccessToken) => {
-  //       setAccessToken(newAccessToken);
-  //       if (!newAccessToken) {
-  //         // apolloSetting에서도 getAccessToken을 하고있어서 두번 요청이 가게 된다. 비효율적이다.
-  //         alert("로그인 화면으로 이동합니다.");
-  //         router.push("/login");
-  //       }
-  //     });
-  //   }
-  // }, []);
 
   useEffect(() => {
     if (!accessToken) {
@@ -71,6 +21,18 @@ export const withAuth = (Component) => (props) => {
       });
     }
   }, []);
+
+  /* 글로벌 프로미스 방식 */
+  // useEffect(() => {
+  //   if (!accessToken) {
+  //     restoreAccessToken.toPromise().then((newAccessToken) => {
+  //       if (!newAccessToken) {
+  //         alert("로그인 후 이용해주세요.");
+  //         router.push("/login");
+  //       }
+  //     });
+  //   }
+  // }, []);
 
   return <Component {...props} />;
 };
