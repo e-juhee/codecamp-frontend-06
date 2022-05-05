fa16aec5db45717a1e139bef809d73624e0e2235
e-juhee <82787570+e-juhee@users.noreply.github.com>
2022-05-05 14:42:03 +0900

    build 2

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "gboard",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
